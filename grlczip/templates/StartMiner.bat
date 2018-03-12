482bb025d19801e128246f812ab6d86e74a437e6
Christian Gerlach <christianrgerlach@gmail.com>
2018-03-12 15:41:05 -0700

    fixed sgminer

--- grlczip/templates/StartMiner.bat
+++ grlczip/templates/StartMiner.bat
@@ -1,1 +1,5 @@
-.\miner\{{ miner }} --algo=allium -o stratum+tcp://drink.garlic.wine:3333 -u {{ address }}
+{% if miner == 'sgminer' %}
+	.\miner\sgminer --algorithm allium -o stratum+tcp://eu.stratum.garlicpool.org:3333 -u ADDRESS -p x -I 14
+{% else %}
+	.\miner\{{ miner }} --algo=allium -o stratum+tcp://drink.garlic.wine:3333 -u {{ address }}
+{% endif %}
